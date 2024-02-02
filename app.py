520ccba90d12b936fed0e46efc486ed99e253824
Jianlin Ye <ye.jianlin@ucy.ac.cy>
2024-02-02 14:29:05 +0200

    added files for the demo

---
+++ app.py
@@ -0,0 +1,13 @@
+import gradio as gr
+from transformers import pipeline
+
+pipe = pipeline(task="image-classification", 
+                # model that can do category classification
+                # https://huggingface.co/microsoft/beit-base-patch16-384
+                model = "microsoft/beit-base-patch16-384")
+gr.Interface.from_pipeline(pipe, 
+                           title="Image Classification",
+                           description="Object Recognition using Microsoft BEIT",
+                           examples = ['egyptian_cat.jpg', 'German_shepherd.jpg',],
+                           article = "Author: <a href=\"https://huggingface.co/jye9\">Jianlin</a>",
+                           ).launch()
